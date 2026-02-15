aed8b0a08e7f68a33e213aecd359a8bd2a7777f2
Tim Finch <32274695+Peippo1@users.noreply.github.com>
2026-02-15 16:55:43 +0000

    Initial Commit

---
+++ TripLingo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TripLingo
+//
+//  Created by Tim Finch on 15/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
